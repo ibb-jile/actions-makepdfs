21ee536408384372c672892848c2d4f22546611d
jiriletak <jiri.letak@ibillboard.com>
2020-06-02 07:30:36 +0200

    + hightlight.js

--- makepdfs.js
+++ makepdfs.js
@@ -7,9 +7,10 @@ const output_dir = process.env.INPUT_OUTPUT_DIR;
 var fs = require( 'fs' );
 var mddir = '/github/workspace/' + markdown_dir;
 var dir = '/github/workspace/' + output_dir + '/';
+var hljs = 'highlight';
 
-/* 
- * Show an error message 
+/*
+ * Show an error message
  */
 function showErrorMessage(msg, error) {
 	console.log('ERROR: ' + msg);
@@ -28,7 +29,7 @@ function makeHtml(data) {
 		var template = fs.readFileSync("/template/template.html").toString('utf-8');
 		// compile template
 		var mustache = require('mustache');
-  
+
 		var view = {
 			style: style,
 			content: data
@@ -66,7 +67,7 @@ function makePdf(data,file) {
 				footerTemplate: '<div class="pageNumber" style="font-size:8px;width:100%;text-align:center;"></div>',
 				headerTemplate: '<div class="date" style="font-size:8px;width:100%;text-align:center;"></div>'
 			});
-		  
+
 			await browser.close();
 		  })();
 	} catch (error) {
@@ -96,7 +97,7 @@ if (!fs.existsSync(dir)){
 
 fs.readdir (mddir,function(err, files) {
    for (let file of files) {
-      if (path.extname(file) == '.md') {				 
+      if (path.extname(file) == '.md') {
 				var text = fs.readFileSync(mddir + '/' + file).toString('utf-8');
 				var md = require('markdown-it')({
 					html: true,
@@ -107,7 +108,7 @@ fs.readdir (mddir,function(err, files) {
 							str = hljs.highlight(lang, str, true).value;
 						} catch (error) {
 							str = md.utils.escapeHtml(str);
-				
+
 							showErrorMessage('markdown-it:highlight', error);
 						}
 						} else {
